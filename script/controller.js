4d3e463a05a253889048cdf3e152f4ddff9cdbe0
todo87 <todoras.stefan@yahoo.com>
2015-05-09 22:38:13 +0300

    added_skill_matrix
    skill matrix added

--- script/controller.js
+++ script/controller.js
@@ -1,20 +1,42 @@
 mainApp.controller("controller", function($scope,$http,$sce,get) {
 
-  $scope.personalData = null;
-  $scope.personalDataKeys = null;
-  $scope.education = null;
-  $scope.educationKeys = null;
-
+  $scope.personalData = {};
+  $scope.education = {};
+  $scope.proExp = {};
+  $scope.skillMatrixHead = ["Category", "Item", "No. of years", "Last year used", "Skill level", "Remarks (versions used, certifications, other remarks)"];
+  $scope.matrix = {};
+  $scope.os = [{}];
+  
   get.async('personalData.json').then(function(d) {
     $scope.personalData = angular.fromJson(d);
-    $scope.personalDataKeys = Object.keys(d);
   });
 
   get.async('education.json').then(function(d) {
-    $scope.education = d;
-    $scope.educationKeys = Object.keys(d);
-    $scope.education["Formal Qualifications"] = $sce.trustAsHtml(d["Formal Qualifications"]);
-
+    $scope.education = angular.fromJson(d);
   });
+  
+  get.async('proExp.json').then(function(d) {
+    $scope.proExp = angular.fromJson(d);
+  });
+  
+  get.async('matrix.json').then(function(d) {
+    $scope.matrix = angular.fromJson(d);
+  });
+
+  $scope.isArray = function(obj){
+    return angular.isArray(obj);
+  }
+  
+  $scope.isObject = function(obj){
+    return angular.isObject(obj);
+  }
+
+  $scope.getKeys = function(obj){
+	var retVal = [];
+	if(obj != null || obj != undefined){
+		retVal = Object.keys(obj);
+	}	 
+    return retVal;
+  }
 
 });
