c0808ba45d18110fecb1df6952b206eda0bb59f0
todo87 <todoras.stefan@yahoo.com>
2015-10-04 21:35:15 +0300

    change alignment

--- script/get.js
+++ script/get.js
@@ -1,16 +1,16 @@
-mainApp.factory('get', function($http) {
-  var myService = {
-    async: function(object) {
-      // $http returns a promise, which has a then function, which also returns a promise
-      var promise = $http.get('script/data/'+ object).then(function (response) {
-        // The then function here is an opportunity to modify the response
-        console.log(response);
-        // The return value gets picked up by the then in the controller.
-        return response.data;
-      });
-      // Return the promise to the controller
-      return promise;
-    }
-  };
-  return myService;
+mainApp.factory('get', function ($http) {
+    var myService = {
+        async: function (object) {
+            // $http returns a promise, which has a then function, which also returns a promise
+            var promise = $http.get('script/data/' + object).then(function (response) {
+                // The then function here is an opportunity to modify the response
+                console.log(response);
+                // The return value gets picked up by the then in the controller.
+                return response.data;
+            });
+            // Return the promise to the controller
+            return promise;
+        }
+    };
+    return myService;
 });
